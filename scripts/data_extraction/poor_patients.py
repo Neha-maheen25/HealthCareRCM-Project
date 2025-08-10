3f39155ff1e5325492c1287632bf47b7f1c81719
Neha-maheen25 <nehamaheenafgan@gmail.com>
2025-08-10 13:42:03 +0530

    Description on what poor_patients.py does

--- scripts/data_extraction/poor_patients.py
+++ scripts/data_extraction/poor_patients.py
@@ -1,3 +1,4 @@
+#this script is used for for testing and internal query during the process consider it as testing script
 import pandas as pd
 df = pd.read_csv(r"C:\Users\hp\OneDrive\Desktop\rcm_project\outputs\transformed_outputs\cleaned_patients.csv")
 print(df.columns.tolist())
